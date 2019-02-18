68f8c63d243a186816c83c780956c3966ab7372c
吴浪 <wul@koal.com>
2019-02-18 17:07:46 +0800

    增加删除操作

--- src/main/java/com/springboot/demo/application/controller/BookController.java
+++ src/main/java/com/springboot/demo/application/controller/BookController.java
@@ -4,6 +4,7 @@ import com.springboot.demo.application.mod.Book;
 import com.springboot.demo.application.service.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,5 +22,11 @@ public class BookController {
         List<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
         return "list";
+    }
+
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable Long id){
+        bookRepository.deleteById(id);
+        return "redirect:/all";
     }
 }
